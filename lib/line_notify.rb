34967999d632bd0228d1fafea6c702e8228630f7
ryu.morikawa <ryu.morikawa@livesense.co.jp>
2016-10-05 17:37:17 +0900

    fix bug

--- lib/line_notify.rb
+++ lib/line_notify.rb
@@ -4,7 +4,14 @@ require 'faraday'
 module LineNotify
   class Client
     LINE_NOTIFY_API_HOST = 'https://notify-api.line.me'
-    TOKEN = 'set your token'
+
+    def self.access_token=(token)
+      @@token = token
+    end
+
+    def self.access_token
+      @@token
+    end
 
     def initialize
       @connection = Faraday::Connection.new(url: LINE_NOTIFY_API_HOST)
@@ -14,7 +21,7 @@ module LineNotify
       response = @connection.post do |req|
         req.url 'api/notify'
         req.headers['Content-Type'] = 'application/x-www-form-urlencoded'
-        req.headers['Authorization'] = "Bearer #{TOKEN}"
+        req.headers['Authorization'] = "Bearer #{self.class.access_token}"
         req.body = {message: message}
       end
       response.body
